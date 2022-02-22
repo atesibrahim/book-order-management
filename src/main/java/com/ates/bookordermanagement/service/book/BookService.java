5a967e4bcea60caae8840aad3a35d381e2d29c03
atesibrahim <ates.ibrahimhalil@gmail.com>
2022-02-22 17:55:00 +0300

    removed spaces

--- src/main/java/com/ates/bookordermanagement/service/book/BookService.java
+++ src/main/java/com/ates/bookordermanagement/service/book/BookService.java
@@ -7,15 +7,9 @@ import com.ates.bookordermanagement.service.model.BookModel;
 import com.ates.bookordermanagement.utils.SufficientException;
 
 public interface BookService {
-	
 	public List<BookModel> getAllBooks();
-	
 	public BookModel getBookInfo(Long id);
-	
 	public BookModel addBook(BookModel bookModel);
-	
 	public Boolean deleteBook(Long id);
-	
 	public BookEntity checkBookCount(Long bookId, int bookCount) throws SufficientException;
-
 }
